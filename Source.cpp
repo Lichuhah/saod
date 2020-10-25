2e73d7630d5e8713b99bfa2f9fd0b02fbd3fb997
Lichuha <belovanton.work@gmail.com>
2020-10-26 02:23:21 +0300

    lab4.3

--- Source.cpp
+++ Source.cpp
@@ -2,32 +2,24 @@
 #include<string>
 #include<ctime>
 #include <fstream>
+#include<vector>
 
 using namespace std;
 
+const int sectorsamount = 10000;
+const int keyamount = 10000;
 
+/////////////////////////////хэш-функция//////////////////////////////////////////
 int doKey(string key) {
 	int keyint;
-	keyint = ((int)key[0] * (int)key[0] + (int)key[1] * (int)key[1] + (int)key[2] * (int)key[2] + (int)key[3] * (int)key[3] + (int)key[4] * (int)key[4] + (int)key[5] * (int)key[5]) % 2000;
+	keyint = ((int)key[0] * (int)key[0] + (int)key[1] * (int)key[1] + (int)key[2] * (int)key[2] + (int)key[3] * (int)key[3] + (int)key[4] * (int)key[4] + (int)key[5] * (int)key[5]) % sectorsamount;
 	return keyint;
 }
 
-void addKeys(string count, int key, string table[2000]) {
-	bool bad = true;
-	for (int i = key; i < 2000; i++) {
-		if (table[i].empty()) {
-			table[i] = count;
-			bad = false;
-			break;
-		}
-	}
-	if (bad) {
-		
-	}
-}
 
-void genKeys(string table[2000]) {
-	for (int i = 0; i < 6000; i++)
+/////////////////////////////куча значений и их просмотр//////////////////////////////////////////
+void genKey(string key[keyamount]) {
+	for (int i = 0; i < keyamount; i++)
 	{
 		string temp;
 		temp += (char)(rand() % 10 + 48);
@@ -36,66 +28,173 @@ void genKeys(string table[2000]) {
 		temp += (char)(rand() % 26 + 65);
 		temp += (char)(rand() % 26 + 65);
 		temp += (char)(rand() % 10 + 48);
+		key[i] = temp;
+	}
+}
+
+void showKey(string key[keyamount]) {
+	for (int i = 0; i < keyamount; i++)
+	{
+		cout << key[i] << " ";
+	}
+}
+
+
+/////////////////////////////таблица для проверки и её просмотр/////////////////////////////////////////
+struct Key {
+	vector<string> counts;
+};
+
+void createTableForCheck(Key tableForCheck[sectorsamount], string key[keyamount]) {
+	for (int i = 0; i < keyamount; i++)
+	{
+		string temp = key[i];
 		int keytemp = doKey(temp);
-		addKeys(temp, keytemp, table);
-			
+		tableForCheck[keytemp].counts.push_back(temp);
 	}
 }
 
-void showTable(string table[2000]) {
-	for (int i = 0; i < 2000; i++) {
-		string temp = table[i];
-		if (temp.empty()) {
-			cout << i << ": " << "NULL"<<endl;
+void showTableForCheck(Key tableForCheck[sectorsamount]) {
+	for (int i = 0; i < sectorsamount; i++) {
+		cout << i << ": ";
+		for (int j = 0; j < tableForCheck[i].counts.size(); j++) {
+			cout << tableForCheck[i].counts[j] << " ";
 		}
-		else {
-			cout << i << ": "<<temp<<endl;
-		}
+		cout << endl;
 	}
 }
 
-void search(string count, string table[2000]) {
-	int temp = doKey(count);
-	for (int i = temp; !table[i].empty(); i++) {
-		if (table[i] == count) {
-			cout << i << ":" << count<<endl;
-			break;
-		}
-	}
-}
-
-void search(int key, string table[2000]) {
-	cout << key << ":" << table[key] << endl;
-}
-
-void outExcel(string table[2000]) {
+void outExcel(Key tableForCheck[sectorsamount]) {
 	std::ofstream out("out.txt");
 	std::streambuf* coutbuf = std::cout.rdbuf();
 	std::cout.rdbuf(out.rdbuf());
-	
-	for (int i = 0; i < 2000; i++)
-	{
-		if (table[i].empty()) {
-			cout << i << " " << 0 << endl;
-		}
-		else {
-			cout << i << " " << 1 << endl;
-		}
-		
+	for (int i = 0; i < sectorsamount; i++) {
+		int lenght = tableForCheck->counts.size();
+		cout << i << " " << lenght << endl;
 	}
 }
+
+
+/////////////////////////////хэш-таблица, ее просмотр, добавление и удаление элемента//////////////////////////////////////////
+void addCount(string count, string table[sectorsamount], Key badKey[sectorsamount]){
+	bool bad = true;
+	int tempkey = doKey(count);
+	if (table[tempkey].empty()) {
+		table[tempkey] = count;
+	}
+	else {
+		bad = true;
+		for (int j = tempkey; j < sectorsamount; j++) {
+			if (table[j].empty()) {
+				table[j] = count;
+				bad = false;
+				break;
+			}
+		}
+		if (bad) {
+			for (int j = 0; j < tempkey; j++) {
+				if (table[j].empty()) {
+					table[j] = count;
+					bad = false;
+					break;
+				}
+			}
+		}
+		if (bad) {
+			badKey[tempkey].counts.push_back(count);
+		}
+	}
+}
+
+void createTable(string table[sectorsamount], string key[keyamount], Key badKey[sectorsamount]) {
+	for (int i = 0; i < keyamount; i++) {
+		string tempcount = key[i];
+		addCount(tempcount, table, badKey);
+	}
+}
+
+void showTable(string table[sectorsamount]) {
+	for (int i = 0; i < sectorsamount; i++) {
+		cout << i << ": " << table[i] << endl;
+	}
+}
+
+void findByValue(string count, string table[sectorsamount]) {
+	bool nofind = true;
+	int tempkey = doKey(count);
+	if (table[tempkey] == count) {
+		cout << tempkey;
+	}
+	else {
+		for (int i = tempkey; i < sectorsamount; i++) {
+			if (table[i] == count) {
+				cout << i;
+				nofind = false;
+				break;
+			}
+		}
+		if (nofind) {
+			cout << "ну-ка нахуй ты где";
+			for (int i = 0; i < tempkey; i++) {
+				if (table[i] == count) {
+					cout << i;
+					nofind = false;
+					break;
+				}
+			}
+		}
+		if (nofind) {
+			cout << "Такого элемента нет";
+		}
+	}
+}
+
+void deleteElement(int key, string table[sectorsamount], Key badKey[sectorsamount]) {
+	table[key].clear();
+	if (badKey[key].counts.empty()) {
+		for (int i = 0; i < sectorsamount; i++) {
+			if (!badKey[i].counts.empty()) {
+				table[key] = badKey[i].counts[0];
+				badKey[i].counts.erase(badKey[i].counts.begin());
+				break;
+			}
+		}
+	}
+	else {
+		table[key] = badKey[key].counts[0];
+		badKey[key].counts.erase(badKey[key].counts.begin());
+	}
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
 
 	srand(time(0));
-	string table[2000];
-	//addKeys("2FGHF1", doKey("2FGHF1"), table);
-	//addKeys("1FGHF2", doKey("1FGHF2"), table);
-	genKeys(table);
+	string key[keyamount];
+	Key keyTableForCheck[sectorsamount];
+	Key badKey[sectorsamount];
+	string table[sectorsamount];
+	genKey(key);
+	//showKey(key);
+//	cout << endl << "______________________________________________________" << endl;
+//	createTableForCheck(keyTableForCheck, key);
+	//showTableForCheck(keyTableForCheck);
+//	cout << endl << "______________________________________________________" << endl;
+	createTable(table, key, badKey);
+	//showTable(table);
+	cout << endl << "______________________________________________________" << endl;
+//	showTableForCheck(badKey);
+	cout << table[rand() % sectorsamount];
+	string a;
+	cin >> a;
+	findByValue(a, table);
+	/*int a;
+	cin >> a;
+	deleteElement(a, table, badKey);
+	cout << endl << "______________________________________________________" << endl;
 	showTable(table);
-	outExcel(table);
-	//search("1FGHF2", table);
-	
-
-	
+	cout << endl << "______________________________________________________" << endl;
+	showTableForCheck(badKey);*/
 	
 }
